18eef12a94bc28b1b376028a420a1e6472cac269
Ibrian Mihai-Razvan <razvanibrian@gmail.com>
2023-06-11 22:40:38 +0300

    implemented pagination on users table
    Signed-off-by: Ibrian Mihai-Razvan <razvanibrian@gmail.com>

--- src/app/components/admin-users-table/admin-users-table.component.ts
+++ src/app/components/admin-users-table/admin-users-table.component.ts
@@ -5,45 +5,44 @@ import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
-
-
-
 @Component({
   selector: 'app-admin-users-table',
   templateUrl: './admin-users-table.component.html',
   styleUrls: ['./admin-users-table.component.css'],
 })
 
-export class AdminUsersTableComponent implements OnInit, OnDestroy,AfterViewInit  {
+export class AdminUsersTableComponent implements OnInit, OnDestroy, AfterViewInit {
 
-
-
-
-
-
-  // tableScriptElement: HTMLScriptElement;
-  constructor(private userService: UserService) {
-    // this.tableScriptElement = document.createElement("script");
-    // this.tableScriptElement.src = "assets/users-table.js";
-    // document.body.appendChild(this.tableScriptElement);
-  }
+  constructor(private userService: UserService) { }
 
   userSubscription: Subscription = new Subscription();
-  userList: User[] = []; // Initializam o lista goala care contine obiecte de tip User. Aceasta va fi populata la ngOnInit cu ajutorul metodei getUsers()
-  columnsToDisplay = ['id', 'department', 'team', 'role', 'name', 'email', 'phone', 'action'];
 
-  
 
-  isLoading = false;
+
+  // SETARI TABEL 
+  // userList: User[] = [];  Initializam o lista goala care contine obiecte de tip User. Aceasta va fi populata la ngOnInit cu ajutorul metodei getUsers()
+  dataSource = new MatTableDataSource<User>([]); // Initializam o lista goala care contine obiecte de tip User, sub forma MatTableDataSource ca sa poata fi paginabila si filtrabila de catre angular-material
+
+  columnsToDisplay = ['id', 'department', 'team', 'role', 'name', 'email', 'phone', 'action']; // Aici se specifica elementului html mat-table ce coloane din typescript sa se afiseze
+
+  @ViewChild(MatPaginator) paginator: any = MatPaginator; // Initializeaza paginator-ul din angular material
+
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+    setTimeout(() => this.dataSource.paginator = this.paginator, 40);
+  }
+
+
+
+
+  isLoading = false; // pentru a astepta incarcarea paginii, adica pentru a astepta pana cand se incarca toti utilizatorii
   isModalOpen = false; // Formularul de adaugare utilizatori noi este prestabilit ascuns
-
-
-
 
   ngOnInit(): void {
     // console.log('Admin homepage has been initialized');
     this.getUsers();
   }
+
 
   // apeleaza functia getUsers() din serviciul user injectat si populeaza lista existenta
   getUsers() {
@@ -52,7 +51,10 @@ export class AdminUsersTableComponent implements OnInit, OnDestroy,AfterViewInit
     this.userSubscription =
       this.userService.getUsers().subscribe((responseUserList) => {
         // console.log(responseUserList);
-        this.userList = responseUserList;
+        // this.userList = responseUserList;
+
+        this.dataSource = new MatTableDataSource(responseUserList); // atribuim rezultatul request-ului listei ce va fi afisata in mat-table
+
         this.isLoading = false; // doar dupa ce se vor finaliza intructiunile time consuming variabila isLoading va fi setata inapoi pe false
       })
   }
@@ -62,12 +64,6 @@ export class AdminUsersTableComponent implements OnInit, OnDestroy,AfterViewInit
   }
   closeModal() {
     this.isModalOpen = false;
-  }
-
-  @ViewChild('paginator') paginator!: MatPaginator;
-  dataSource = new MatTableDataSource<User>(this.userList);  
-  ngAfterViewInit() {
-    this.dataSource.paginator=this.paginator;
   }
 
   ngOnDestroy(): void {
