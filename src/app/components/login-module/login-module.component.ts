b3f78b8a025ad2fe9867ca3a9ea876df60c3f488
Ibrian Mihai-Razvan <ibrianmihairazvan@yahoo.com>
2023-06-14 01:42:27 +0300

    implemented login method
    Signed-off-by: Ibrian Mihai-Razvan <ibrianmihairazvan@yahoo.com>

--- src/app/components/login-module/login-module.component.ts
+++ src/app/components/login-module/login-module.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+
+
 
 @Component({
   selector: 'app-login-module',
@@ -6,5 +12,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-module.component.css']
 })
 export class LoginModuleComponent {
+  form = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl(null, Validators.required)
+  });
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  submitForm() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.authService
+      .login(this.form.get('email')?.value, this.form.get('password')?.value)
+      .subscribe((response) => {
+        this.router.navigate(['/admin']);
+      })
+  }
 
 }
