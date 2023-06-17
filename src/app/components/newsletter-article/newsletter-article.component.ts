478afd1cd38c2eb0643acc32952ce05932345f93
TVasluianu <tudor.vasluianu@gmail.com>
2023-06-17 19:48:22 +0300

    Created the Search by title functionality for articles in the newsfeed page

--- src/app/components/newsletter-article/newsletter-article.component.ts
+++ src/app/components/newsletter-article/newsletter-article.component.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { NewsletterService } from 'src/app/services/newsletter.service';
 import { AddArticleModalComponent } from '../add-article-modal/add-article-modal.component';
 
-// needed for the search functionality 
+// to filter by title used this tutorial: https://www.youtube.com/watch?v=lTOQ7Fjhcvk
 
 @Component({
   selector: 'app-newsletter-article',
@@ -13,6 +13,18 @@ import { AddArticleModalComponent } from '../add-article-modal/add-article-modal
 })
 export class NewsletterArticleComponent implements OnInit {
   articles: NewsletterArticle[] = [];
+  _filterText: string = ''; 
+  filteredArticles: NewsletterArticle [] = []; 
+
+
+  get filterText(){
+    return this._filterText;
+  }
+  
+  set filterText(value: string){
+    this._filterText = value; //equal to what we add in the text box
+    this.filterArticles(); //whenever the text is changed we reimplement filterArticles() also due to NgModel we get the result of this function
+  }
   
 
   constructor(
@@ -20,6 +32,7 @@ export class NewsletterArticleComponent implements OnInit {
     public dialog: MatDialog
   ) {}
   ngOnInit() {
+    
     console.log('Newsletter article component works');
     this.getArticles();
   }
@@ -27,6 +40,7 @@ export class NewsletterArticleComponent implements OnInit {
   getArticles() {
     this.articlesService.getNewsletterArticles().subscribe((response) => {
       this.articles = response.reverse(); // nu e good practice si ar trebui din backend sa sortam aceste articole ca cel nou sa fie in fata
+      this.filteredArticles = this.articles; // we make sure that filteredArticles array is a copy of articles that we actually show in our html
       console.log(response);
     });
   }
@@ -56,4 +70,17 @@ export class NewsletterArticleComponent implements OnInit {
       this.getArticles()
     });
   }
+//filters the articles and keeps the ones where what we add in the textbox exists in an article title
+  filterArticles(){
+    if(this.articles.length === 0 || this._filterText === ''){
+      this.filteredArticles = this.articles; // if the textbox is empty or we have no articles we keep the status quo
+    } else {
+      //found here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#searching_in_array
+      this.filteredArticles = this.articles.filter((article) => //filter function creates a new array which includes only the items that match the filter - article is a parameter that reprisents one item of the articles array
+      { 
+          return article.title.toLowerCase().includes(this._filterText.toLowerCase()); // we return each article that includes whats written in the textbox
+      })
+    }
+  
+}
 }
