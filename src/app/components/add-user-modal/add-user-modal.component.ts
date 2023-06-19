daea6e7a78a559b139ddca73330897d592204b4f
Ibrian Mihai-Razvan <razvanibrian@gmail.com>
2023-06-19 22:32:13 +0300

    implemented form control for add/edit user modal
    Signed-off-by: Ibrian Mihai-Razvan <razvanibrian@gmail.com>

--- src/app/components/add-user-modal/add-user-modal.component.ts
+++ src/app/components/add-user-modal/add-user-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, SecurityContext } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer} from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
@@ -12,76 +13,25 @@ import { UserService } from 'src/app/services/user.service';
 export class AddUserModalComponent implements OnDestroy, OnInit {
   @Output() newCloseModalEvent = new EventEmitter<string>();
   @Output() newGetUsersEvent = new EventEmitter<string>();
+
   @Input() editedUser: User = new User();
   @Input() modalType: String = '';
-  @Input() modalRole: String = ''; 
-  roles: string[] = ['employee','hr','admin'];
-  selectedRole: string= '';
-
-  // Creem formularul si campurile acestuia
-  userForm = new FormGroup({
-
-    department: new FormControl('',[
-      Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(10),
-      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
-    ]),
-
-    function: new FormControl('',[
-      Validators.required,
-      Validators.minLength(7),
-      Validators.maxLength(30),
-      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
-    ]),
-
-    role: new FormControl('',[
-      Validators.required,
-    ]),
-
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(50),
-      Validators.pattern(/^[a-zA-Z\s]*$/) // only alphabetic
-    ]),
-
-    email: new FormControl('',[
-      Validators.required,
-      Validators.minLength(7),
-      Validators.maxLength(60),
-      Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) // email format
-    ]),
-
-    phone: new FormControl('',[
-      Validators.required,
-      Validators.minLength(10),
-      Validators.maxLength(10),
-      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
-    ]),
-
-    password: new FormControl('',[
-      Validators.required,
-      Validators.minLength(7),
-      Validators.maxLength(60),
-      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
-    ])
-  })
-
-  onRoleChange() {
-    this.selectedRole = this.userForm.value.role || "";
-    console.log('Selected Role:', this.selectedRole);
-  }
-
-  addUserSubscription: Subscription = new Subscription();
-  updatedUserSubscription: Subscription = new Subscription();
+  @Input() modalRole: String = '';
 
   particlesScriptElement: HTMLScriptElement;
   particlesSettingsScriptElement: HTMLScriptElement;
   particlesHostingElement: HTMLScriptElement;
 
+  addUserSubscription: Subscription = new Subscription();
+  updatedUserSubscription: Subscription = new Subscription();
 
-  constructor(private userService: UserService) {
+  roles: string[] = ['employee', 'hr', 'admin'];
+  selectedRole: string = '';
+  errorMessage: string = '';
+  errorSource: string = '';
+
+
+  constructor(private userService: UserService, private sanitizer: DomSanitizer) {
     // Injectam serviciul user pentru a putea folosii metodele din acesta (put si post http requests in cazul nostru)
 
     // adaugam in mod dinamic fisierul ce contine logica pentru fundalul animat, particle.js (din folder-ul assets al angular) la HTML-ul componentei
@@ -101,10 +51,60 @@ export class AddUserModalComponent implements OnDestroy, OnInit {
   }
 
 
+  // Creem formularul si campurile acestuia, cu restrictiile specifice
+  userForm = new FormGroup({
+
+    department: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(10),
+      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
+    ]),
+
+    function: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      Validators.maxLength(30),
+      Validators.pattern(/^[a-zA-Z0-9\s]*$/) // only alphanumeric
+    ]),
+
+    role: new FormControl('', [
+      Validators.required,
+    ]),
+
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+      Validators.pattern(/^[a-zA-Z\s]*$/) // only alphabetic
+    ]),
+
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7), 
+      Validators.maxLength(60), 
+      Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) // email format
+    ]),
+
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern('^[0-9]+$') // only numeric
+    ]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+      Validators.maxLength(60),
+    ])
+  })
+
+
   ngOnInit(): void {
     // Daca modala a fost apelata de pe butonul Edit, initializam formularul completat cu datele utilizatorului ce urmeaza a fi editat. 
     // Daca modala a fost apelata de pe butonul Add, initializam formularul cu campurile goale 
-    console.log('modal type: '+this.modalType);
+    console.log('modal type: ' + this.modalType);
     this.userForm.patchValue({
       department: this.editedUser.department,
       function: this.editedUser.function,
@@ -119,15 +119,182 @@ export class AddUserModalComponent implements OnDestroy, OnInit {
 
   closeModal() {
     this.newCloseModalEvent.emit();
-    // Functia closeModal() va emite un eveniment pe nume newCloseModalEvent ce va fi receptionat de catre componenta parinte (admin-home-page) si va inchide modala
+    // Functia closeModal() va emite un eveniment pe nume newCloseModalEvent ce va fi receptionat de catre componenta parinte si va inchide modala
   }
-  
 
 
+  sanitizeInput(input: string): string {
+    // Sanitize the input to prevent XSS
+    const sanitizedInput = this.sanitizer.sanitize(SecurityContext.HTML, input);
+    // Return the sanitized input
+    return sanitizedInput || '';
+  }
+
+  // Se apeleaza cand formularul produce o eroare. Verifica despre ce eroare este vorba si afiseaza un mesaj specific pentru aceasta.
+  handleUserFormError() { 
+    // !! = obtine valoarea boolean a unui element
+
+    switch (true) {
+
+      // Checking if errors come from department field
+      case !!this.userForm.controls.department.errors:
+        this.errorSource = 'department';
+        switch (true) {
+          case !!this.userForm.controls.department.errors?.['required']:
+            this.errorMessage = 'Every user must be part of a department';
+            break;
+          case !!this.userForm.controls.department.errors?.['pattern']:
+            this.errorMessage = 'Department name can contain only aphanumeric characters.';
+            break;
+          case !!this.userForm.controls.department.errors?.['minlength']:
+            this.errorMessage = 'Department must contain at least 2 characters.';
+            break;
+          case !!this.userForm.controls.department.errors?.['maxlength']:
+            this.errorMessage = 'Department can contain a maximum of 10 characters.';
+            break;
+          default:
+            this.errorMessage = 'Something went wrong.';
+            break;
+        }
+        break;
+
+      // Checking if errors come from function field
+      case !!this.userForm.controls.function.errors:
+        this.errorSource = 'function';
+        switch (true) {
+          case !!this.userForm.controls.function.errors?.['required']:
+            this.errorMessage = 'Every user must be have a function';
+            break;
+          case !!this.userForm.controls.function.errors?.['pattern']:
+            this.errorMessage = 'Function name can contain only aphanumeric characters.';
+            break;
+          case !!this.userForm.controls.function.errors?.['minlength']:
+            console.log('min length error')
+            this.errorMessage = 'Function must contain at least 7 characters.';
+            break;
+          case !!this.userForm.controls.function.errors?.['maxlength']:
+            this.errorMessage = 'Function can contain a maximum of 30 characters.';
+            break;
+          default:
+            if (this.userForm.controls.function.errors) {
+              this.errorMessage = 'Something went wrong with the function field.';
+            } else {
+              this.errorMessage = 'Something went wrong.';
+            }
+            break;
+        }
+        break;
+
+      // Checking if errors come from role field
+      case !!this.userForm.controls.role.errors:
+        this.errorSource = 'role';
+        this.errorMessage = 'Every user must have a role.'
+        break;
+
+      // Checking if errors come from name field
+      case !!this.userForm.controls.name.errors:
+        this.errorSource = 'name';
+        switch (true) {
+          case !!this.userForm.controls.name.errors?.['required']:
+            this.errorMessage = 'Every user must have a name';
+            break;
+          case !!this.userForm.controls.name.errors?.['pattern']:
+            this.errorMessage = 'User name can contain only alphabetic characters.';
+            break;
+          case !!this.userForm.controls.name.errors?.['minlength']:
+            this.errorMessage = 'User name must contain at least 5 characters.';
+            break;
+          case !!this.userForm.controls.name.errors?.['maxlength']:
+            this.errorMessage = 'User name can contain a maximum of 50 characters.';
+            break;
+          default:
+            this.errorMessage = 'Something went wrong.';
+            break;
+        }
+        break;
+
+      // Checking if errors come from e-mail field
+      case !!this.userForm.controls.email.errors:
+        this.errorSource = 'email';
+        switch (true) {
+          case !!this.userForm.controls.email.errors?.['required']:
+            this.errorMessage = 'Every user must have an e-mail';
+            break;
+          case !!this.userForm.controls.email.errors?.['pattern']:
+            this.errorMessage = 'Invalid e-mail format. Please enter a valid e-mail address.';
+            break;
+          case !!this.userForm.controls.email.errors?.['minlength']:
+            this.errorMessage = 'E-mail must contain at least 7 characters.';
+            break;
+          case !!this.userForm.controls.email.errors?.['maxlength']:
+            this.errorMessage = 'E-mail can contain a maximum of 60 characters.';
+            break;
+          default:
+            this.errorMessage = 'Something went wrong.';
+            break;
+        }
+        break;
+
+      // Checking if errors come from phone field
+      case !!this.userForm.controls.phone.errors:
+        this.errorSource = 'phone';
+        switch (true) {
+          case !!this.userForm.controls.phone.errors?.['required']:
+            this.errorMessage = 'Every user must have a phone number';
+            break;
+          case !!this.userForm.controls.phone.errors?.['pattern']:
+            this.errorMessage = 'Phone number can contain only numeric characters.';
+            break;
+          case !!this.userForm.controls.phone.errors?.['minlength']:
+            this.errorMessage = 'Phone number must contain at least 10 characters.';
+            break;
+          case !!this.userForm.controls.phone.errors?.['maxlength']:
+            this.errorMessage = 'Phone number can contain a maximum of 10 characters.';
+            break;
+          default:
+            this.errorMessage = 'Something went wrong.';
+            break;
+        }
+        break;
+
+      // Checking if errors come from password field
+      case !!this.userForm.controls.password.errors:
+        this.errorSource = 'password';
+        switch (true) {
+          case !!this.userForm.controls.password.errors?.['required']:
+            this.errorMessage = 'Every user must have a password';
+            break;
+          case !!this.userForm.controls.password.errors?.['minlength']:
+            this.errorMessage = 'Password must contain at least 7 characters.';
+            break;
+          case !!this.userForm.controls.password.errors?.['maxlength']:
+            this.errorMessage = 'Password can contain a maximum of 60 characters.';
+            break;
+          default:
+            this.errorMessage = 'Something went wrong.';
+            break;
+        }
+        break;
+
+      // If formular is invalid, but because of reasons different than invalid input
+      default:
+        this.errorSource = 'default';
+        this.errorMessage = 'Something went wrong';
+        break;
+      }
+  }
+
+  // Urmareste valoarea campului role (care este un dropdown cu 3 optiuni)
+  onRoleChange() {
+    this.selectedRole = this.userForm.value.role || "";
+    console.log('Selected Role:', this.selectedRole);
+  }
 
   addUser() {
 
-    if(this.userForm.invalid){
+    // Verificam formularul
+    if (this.userForm.invalid) {
+      this.handleUserFormError();
       return;
     }
 
@@ -139,7 +306,7 @@ export class AddUserModalComponent implements OnDestroy, OnInit {
       name: this.userForm.value.name || '',
       email: this.userForm.value.email || '',
       phone: this.userForm.value.phone || '',
-      password: this.userForm.value.password || ''
+      password: this.sanitizeInput(this.userForm.controls.password.value || "") || ''
     }
 
     // Apelam functia de addUser si ii pasam ca parametru obiectul de tip User creat anterior
@@ -152,6 +319,7 @@ export class AddUserModalComponent implements OnDestroy, OnInit {
   }
 
   updateUser() {
+    // Creem un obiect de tip user cu valorile completate in formular
     const user = {
       id: this.editedUser.id,
       department: this.userForm.value.department || '',
@@ -163,36 +331,13 @@ export class AddUserModalComponent implements OnDestroy, OnInit {
       password: this.userForm.value.password || ''
     }
 
+    // Apelam functia de updateUser() si ii pasam ca parametru obiectul de tip User creat anterior
     this.updatedUserSubscription = this.userService.updateUser(user).subscribe(() => {
       this.newGetUsersEvent.emit();
       this.closeModal();
     })
   }
 
-  getErrorMessage(controlName: string): string {
-    const control = this.userForm.get(controlName);
-  
-    if (control?.errors?.['required']) {
-      return `${controlName} is required.`;
-    }
-  
-    if (control?.errors?.['minlength']) {
-      return `${controlName} must have at least ${control.errors ['minlength'].requiredLength} characters.`;
-    }
-  
-    if (control?.errors?.['maxlength']) {
-      return `${controlName} must have at most ${control.errors ['maxlength'].requiredLength} characters.`;
-    }
-  
-    if (control?.errors?.['pattern']) {
-      return `Please provide a valid ${controlName}.`;
-    }
-  
-    // Add more conditions for other validation errors if needed
-  
-    return '';
-  }
-  
 
   ngOnDestroy(): void {
     this.addUserSubscription.unsubscribe();
