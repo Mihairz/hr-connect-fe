d3128c78ac3045b7fb7d33169348ccb6ab1b05b0
TVasluianu <tudor.vasluianu@gmail.com>
2023-06-22 13:28:18 +0300

    Added pagination to feedback table

--- src/app/components/feedback-form/feedback-form.component.ts
+++ src/app/components/feedback-form/feedback-form.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
 import { Feedback } from 'src/app/models/feedback';
 import { FeedbackFormService } from 'src/app/services/feedback-form.service';
+//https://stackoverflow.com/questions/50995170/angular-mattabledatasource-error
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-feedback-form',
@@ -12,6 +16,7 @@ export class FeedbackFormComponent implements OnInit {
   feedbackForm: FormGroup; //Tracks the value and validity state of a group of FormControl instances.
   feedbacks: Feedback[] = []; // array that will hold the feedback object
   displayedColumns: string[] = ['category', 'title', 'content']; //column of the tabel
+  
 
   constructor(private feedbackService: FeedbackFormService) {
     this.feedbackForm = new FormGroup({
@@ -19,17 +24,34 @@ export class FeedbackFormComponent implements OnInit {
       'title': new FormControl(''),
       'content': new FormControl('')
     });
+   
   }
+  @ViewChild('paginator') paginator!: MatPaginator ;
+  
+  dataSource!: MatTableDataSource<Feedback>;
 
+
+  
+  
   ngOnInit() {
     this.getFeedbacks();
+   
   }
+  ngAfterViewInit() {
+   
+    this.dataSource.paginator = this.paginator;
+}
 
-  getFeedbacks() {
-    this.feedbackService.getFeedback().subscribe((response) => {
-      this.feedbacks = response.reverse();
-    });
-  }
+getFeedbacks() {
+  this.feedbackService.getFeedback().subscribe((response) => {
+    this.feedbacks = response.reverse();
+    this.dataSource = new MatTableDataSource(this.feedbacks); //sets the dataSource property 
+    this.dataSource.paginator = this.paginator; //sets the paginator property 
+  });
+}
+
+
+
 //method when we submit the form 
   saveFeedback() {
     //if the form is valid
